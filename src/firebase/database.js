d9f454e82ccf085bd2400e0747a86ab0d00dc781
abAWDG6 <ab.awgroup6@gmail.com>
2021-03-07 17:41:00 +0000

    Update database with alex config

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -2,17 +2,16 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBWx4p0j16mdUIivZLBqz_8l_BH6OhRpR4",
-    authDomain: "myocardio-awdg6.firebaseapp.com",
-    projectId: "myocardio-awdg6",
-    storageBucket: "myocardio-awdg6.appspot.com",
-    messagingSenderId: "886784373926",
-    appId: "1:886784373926:web:08305df28d2701e4c3b067",
-    measurementId: "G-PK42GS9GFN"
-  };
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional 
+const firebaseConfig = { /**alex config */
+  apiKey: "AIzaSyBiBWMw1OJ1RPs5njdqa7e2nMOZFLpMaZE",
+  authDomain: "myocardio-abaw6.firebaseapp.com",
+  projectId: "myocardio-abaw6",
+  storageBucket: "myocardio-abaw6.appspot.com",
+  messagingSenderId: "50281309479",
+  appId: "1:50281309479:web:a5f386b3fb94e91ba0f502",
+  measurementId: "G-HLBZ4TW612"
+};
   //initialize firebase
   firebase.initializeApp(firebaseConfig);
   const firebaseAuthentication = firebase.auth();
